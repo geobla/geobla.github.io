e988340b5435b857a8ed43acb44ecd1e7a16db4c
geobla <99928380+geobla@users.noreply.github.com>
2022-03-15 07:01:06 +0200

    Add files via upload

---
+++ HTML files/JavaScript/TryIt2.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    document.getElementById("demo2").style.fontSize = "20px";
+    document.getElementById("demo2").style.color = "red";
+    document.getElementById("demo2").style.backgroundColor = "yellow";
+}
